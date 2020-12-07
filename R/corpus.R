84ea0a617eae741f183a8d30ef0ca5929b5c3276
Pozdniakov <bucherr@yandex.ru>
2020-12-07 18:10:22 +0300

    merge by id

--- R/corpus.R
+++ R/corpus.R
@@ -110,7 +110,8 @@ get_corpus <- function(corpus = NULL,
   if (full_metadata) {
     corp_list$dramas <-
       merge(corp_list$dramas,
-            dracor_api(request = paste0(URL, "/metadata"), flatten = TRUE))
+            dracor_api(request = paste0(URL, "/metadata"), flatten = TRUE),
+            by = "id")
     data.table::setcolorder(corp_list$dramas,
                             neworder = c(columns_short_order,
                                          columns_extra_order))
