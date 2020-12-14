a2e0cb7c8ca4d37b9181ed3289e3ac427d0a480a
Pozdniakov <bucherr@yandex.ru>
2020-12-14 19:39:57 +0300

    updated namespace

--- tests/testthat/test-dracor.R
+++ tests/testthat/test-dracor.R
@@ -16,11 +16,13 @@ test_that("several corpora are downloaded via get_dracor() if character vector
             expect_s3_class(get_dracor("tat", "span"), c("dracor", "data.frame"))
           })
 
-test_that("all corpora are loaded via get_dracor() if corpus is equal 'all' and
+test_that(
+  "all corpora are loaded via get_dracor() if corpus is equal 'all' and
           unique corpus names is at least 11",
-          {
-            length(unique(get_dracor()$name)) >= 11
-          })
+  {
+    length(unique(get_dracor()$name)) >= 11
+  }
+)
 
 test_that("is.dracor() works", {
   expect_true(is.dracor(get_dracor("cal")))
@@ -31,9 +33,14 @@ test_that("is.dracor() works", {
 })
 
 test_that("summary() work for 'dracor' object if there is no info on years", {
-  expect_true(all(nchar(capture.output(summary(get_dracor("tat")))) > 3))
+  expect_true(all(nchar(capture.output(
+    summary(get_dracor("tat"))
+  )) > 3))
 })
 
-test_that("writtenYear with format 'YYYY/YYYY' is processed as integer in column writtenYearFinish",{
-  expect_is(get_dracor("rus")$writtenYearFinish, "integer")
-})
+test_that(
+  "writtenYear with format 'YYYY/YYYY' is processed as integer in column writtenYearFinish",
+  {
+    expect_is(get_dracor("rus")$writtenYearFinish, "integer")
+  }
+)
