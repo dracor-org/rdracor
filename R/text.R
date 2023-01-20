45b01f4d61c60337fd4408ce2d9813eac4e7e774
Pozdniakov <bucherr@yandex.ru>
2023-01-20 20:02:41 +0100

    update docs and parameters in get_text_chr_*()

--- R/text.R
+++ R/text.R
@@ -28,12 +28,30 @@ get_text_tei <- function(play = NULL, corpus = NULL, ...) {
 #'
 #' \code{get_text_chr_spoken()} request lines and stage directions for a play,
 #' given play and corpus names.
-#'
 #' @inheritParams get_play_cast
 #' @param gender Character, optional parameter to extract lines for characters
 #' of specified gender: \code{"MALE"}, \code{"FEMALE"}, \code{"UNKNOWN"}.
 #' @param split_text If \code{TRUE} returns text as a character vector of lines.
 #' Otherwise, returns text as one character value. \code{TRUE} by default.
+#' @return For \code{get_text_chr_spoken()}, \code{get_text_chr_stage()} and
+#' \code{get_text_chr_stage_with_sp()}: a character vector (if
+#' \code{split_text = TRUE}, the default value) or a single character value (if
+#' \code{split_text = FALSE)}.
+#' For \code{get_text_chr_spoken_bych()}:
+#' \describe{
+#'   \item{\code{split_text = TRUE} and \code{as_data_frame = FALSE} (default)}{
+#'   a named list with character vectors for every character}
+#'   \item{\code{split_text = FALSE} and \code{as_data_frame = FALSE}}{a named
+#' character vector (one value = one character)}
+#'   \item{\code{split_text = TRUE} and \code{as_data_frame = TRUE}}{a data
+#'   frame: every row represent a character, text of a play is stored in a
+#'   \code{"text"} column, the \code{"text"} column is a list column with a
+#'   character vector of lines}
+#'   \item{\code{split_text = FALSE} and \code{as_data_frame = TRUE}}{a data
+#'   frame: every row represent a character, text of a play is stored in a
+#'   \code{"text"} column, the \code{"text"} column is a simple character
+#'   column}
+#' }
 #' @examples
 #' get_text_chr_spoken(play = "lessing-emilia-galotti", corpus = "ger")
 #' get_text_chr_spoken(
@@ -45,7 +63,7 @@ get_text_tei <- function(play = NULL, corpus = NULL, ...) {
 #'   play = "lessing-emilia-galotti",
 #'   corpus = "ger",
 #'   gender = "FEMALE",
-#'   split = FALSE
+#'   split_text = FALSE
 #' )
 #' get_text_chr_spoken_bych(
 #'   play = "lessing-emilia-galotti",
@@ -67,7 +85,9 @@ get_text_chr_spoken <-
            gender = NULL,
            split_text = TRUE,
            ...) {
-    request <- form_play_request(play = play, corpus = corpus, type = "spoken-text")
+    request <- form_play_request(play = play,
+                                 corpus = corpus,
+                                 type = "spoken-text")
     if (!is.null(gender)) {
       if (!(toupper(gender) %in% c("MALE", "FEMALE", "UNKNOWN"))) {
         stop("gender must be one of 'MALE', 'FEMALE','UNKNOWN'")
@@ -75,11 +95,11 @@ get_text_chr_spoken <-
       request <- paste0(request, "?gender=", toupper(gender))
     }
     dracor_api(request,
-      expected_type = "text/plain", ...
+      expected_type = "text/plain", split_text = split_text, ...
     )
   }
 
-#' @param dataframe If \code{TRUE} returns data frame with a row for every
+#' @param as_data_frame If \code{TRUE} returns data frame with a row for every
 #' character and text in a column \code{"text"}. Otherwise, a named list with
 #' character values is returned. \code{FALSE} by default.
 #' @importFrom purrr map_chr
@@ -87,7 +107,10 @@ get_text_chr_spoken <-
 #' @describeIn get_text_chr_spoken Retrieves lines grouped by characters in a
 #' play, given play and corpus names.
 get_text_chr_spoken_bych <-
-  function(play = NULL, corpus = NULL, split_text = TRUE, dataframe = FALSE, ...) {
+  function(play = NULL,
+           corpus = NULL,
+           split_text = TRUE,
+           as_data_frame = FALSE, ...) {
     play_text <- dracor_api(
       form_play_request(
         play = play,
@@ -100,7 +123,7 @@ get_text_chr_spoken_bych <-
     if (!isTRUE(split_text)) {
       play_text$text <- purrr::map_chr(play_text$text, paste0, collapse = "\n")
     }
-    if (isTRUE(dataframe)) {
+    if (isTRUE(as_data_frame)) {
       return(play_text)
     } else {
       play_text_list <- play_text$text
@@ -113,7 +136,10 @@ get_text_chr_spoken_bych <-
 #' @describeIn get_text_chr_spoken Retrieves all stage directions of a play,
 #' given play and corpus names.
 get_text_chr_stage <-
-  function(play = NULL, corpus = NULL, split_text = TRUE, ...) {
+  function(play = NULL,
+           corpus = NULL,
+           split_text = TRUE,
+           ...) {
     dracor_api(
       form_play_request(
         play = play,
@@ -121,6 +147,7 @@ get_text_chr_stage <-
         type = "stage-directions"
       ),
       expected_type = "text/plain",
+      split_text = split_text,
       ...
     )
   }
@@ -129,7 +156,10 @@ get_text_chr_stage <-
 #' @describeIn get_text_chr_spoken Retrieves all stage directions of a play
 #' including speakers (if applicable), given play and corpus names.
 get_text_chr_stage_with_sp <-
-  function(play = NULL, corpus = NULL, split_text = TRUE, ...) {
+  function(play = NULL,
+           corpus = NULL,
+           split_text = TRUE,
+           ...) {
     dracor_api(
       form_play_request(
         play = play,
@@ -137,6 +167,7 @@ get_text_chr_stage_with_sp <-
         type = "stage-directions-with-speakers"
       ),
       expected_type = "text/plain",
+      split_text = split_text,
       ...
     )
   }
